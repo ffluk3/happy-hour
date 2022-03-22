29a986e6233a55b00828afe1c0144a02999d167a
ffluk3 <lucas.shadler@flockfreight.com>
2022-03-22 16:40:40 -0700

    initial

---
+++ tests/happy-hour.spec.ts
@@ -0,0 +1,42 @@
+import { test, expect } from "@playwright/test";
+
+test("test", async ({ page }) => {
+  // Go to https://wheelofnames.com/95m-aky
+  await page.goto("https://wheelofnames.com/95m-aky");
+
+  // Click #bottomInstruction
+  await page.locator("#bottomInstruction").click();
+
+  // Click text=Home & Away
+  const choice1 = await page.innerText("h1.title > span")
+  console.log(`Option 1: ${choice1}`)
+  
+  // Click button:has-text("Close")
+  await page.locator('button:has-text("Close")').click();
+
+  // Click #wheelCanvas
+  await page.locator("#wheelCanvas").click({
+    position: {
+      x: 302,
+      y: 395,
+    },
+  });
+
+  const choice2 = await page.innerText("h1.title > span")
+  console.log(`Option 2: ${choice2}`)
+
+  // Click button:has-text("Close")
+  await page.locator('button:has-text("Close")').click();
+
+  await page.locator("#wheelCanvas").click({
+    position: {
+      x: 302,
+      y: 395,
+    },
+  });
+
+
+  const choice3 = await page.innerText("h1.title > span")
+  console.log(`Option 3: ${choice3}`)
+
+});
