a98ae283d21fd6a568b8949814b00200dcb0251b
ffluk3 <99682335+ffluk3@users.noreply.github.com>
2022-05-24 17:02:41 -0700

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,14 +11,6 @@ const config: PlaywrightTestConfig = {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
   ],
 };
 export default config;
